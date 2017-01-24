32e9d5c4d89390e1908404cf1ed49d676190523d
akki2825 <akki.kr94@gmail.com>
2017-01-24 15:57:56 +0530

    two layers NN

---
+++ two_layers_neural_net.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+#sigmoid function
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+#Gradient of sigmoid function    
+def _sigmoid_derivative(x):
+    return x*(1-x)
+
+
+#input dataset
+X = np.array([[0,0,1],
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]])
+
+#output dataset                
+y = np.array([[0],
+            [1],
+            [1],
+            [0]])
+
+# Seed the random number generator, so it generates the same number every time the program runs
+np.random.seed(1)
+
+# randomly initialize our weights with mean 0
+w1 = 2*np.random.random((3,4)) - 1
+w2 = 2*np.random.random((4,1)) - 1
+
+
+def predict(inputs):
+        # Pass inputs through our neural network (our single neuron)
+    synaptic_weights = [w1, w2]
+    i1 = inputs
+    for j in range(2):
+        i1 = sigmoid(np.dot(i1, synaptic_weights[j]))
+    return i1
+
+
+#training with alpha
+#have set the value based on trail-and-error
+alpha = 25
+
+for i in xrange(50000):
+
+    # Feed forward through layers 0, 1, 2 and 3
+    l0 = X
+    l1 = sigmoid(np.dot(l0,w1))
+    l2 = sigmoid(np.dot(l1,w2))
+    
+    # how much did we miss the target value?
+    l2_error = y - l2
+    
+    if (i% 5000) == 0:
+        print "Error:" + str(np.mean(np.abs(l2_error)))
+        
+    # in what direction is the target value?
+    # are we really sure? 
+    l2_delta = l2_error*_sigmoid_derivative(l2)
+
+    # how much did each l2 value contribute to the l3 error (according to the weights)?
+    l1_error = l2_delta.dot(w2.T)
+    
+    # in what direction is the target l2?
+    # are we really sure? 
+    l1_delta = l1_error * _sigmoid_derivative(l1)
+
+    w2 += alpha*l1.T.dot(l2_delta)
+    w1 += alpha*l0.T.dot(l1_delta)
+
+print "w1:", w1
+print "w2:", w2
+print "prediction:"
+print predict(np.array([[1,1,1]]))
